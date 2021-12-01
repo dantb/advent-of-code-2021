5623ba4dbf62a35fa590df1403426431ddd4fea1
dantb <dantb@hotmail.co.uk>
2021-12-01 11:33:48 +0000

    Add dev.insideyou scala 3 template project

---
+++ advent-of-code/src/main/scala/dev/dantb/adventofcode/Main.scala
@@ -0,0 +1,9 @@
+package dev.dantb
+package adventofcode
+
+@main def Main(args: String*): Unit =
+  println("─" * 100)
+
+  println("hello world")
+
+  println("─" * 100)
---
+++ advent-of-code/src/test/scala/dev/dantb/adventofcode/ExampleSuite.scala
@@ -0,0 +1,7 @@
+package dev.dantb
+package adventofcode
+
+final class ExampleSuite extends TestSuite:
+  test("hello world") {
+    1 `shouldBe` 1
+  }
